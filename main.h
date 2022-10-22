5c414a4d586657ea50e75bb02d34b47a9dc4ef8c
Dr-Aniekan-Udo <aniekanetimudo@gmail.com>
2022-10-22 00:35:38 -0700

    header file problem attempted to be fixed

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H_
+#define MAIN_H_
 
 #include <stdarg.h>
 #include <unistd.h>
@@ -58,4 +58,4 @@ char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
-#endif
+#endif /* MAIN_H */
